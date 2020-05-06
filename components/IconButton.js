ce32de710bb94c460cd1f64663069bb33ecf57d9
“Mark <markkoenig95@gmail.com>
2020-05-06 09:39:16 +0800

    Reduce use of react-native-elements dependancy

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
-import {Icon} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import styles, {colors} from '../styles/styles';
+import CustomButton from './CustomButton';
 
 export default function IconButton(props) {
   const buttonBackground = styles.button.backgroundColor;
   const buttonText = styles.buttonText.color;
 
+  const size = props.size || 5;
+
+  const buttonStyle = {
+    ...styles.button,
+    ...props.buttonStyle,
+    backgroundColor: !props.invertColor ? buttonBackground : colors.lightGray,
+    borderRadius: size * 5,
+    height: size * 10,
+    width: size * 10,
+    padding: size * 2,
+    margin: size * 2,
+  };
+
+  const iconStyle = {
+    ...styles.buttonText,
+    ...props.iconStyle,
+    fontSize: size * 6,
+    color: !props.invertColor ? buttonText : buttonBackground,
+  };
+
   return (
-    <Icon
-      reverse
-      reverseColor={!props.invertColor ? buttonText : buttonBackground}
-      name={props.name}
-      type={props.type || 'material'}
-      color={!props.invertColor ? buttonBackground : colors.lightGray}
-      containerStyle={props.containerStyle}
-      iconStyle={[{fontSize: 30}, props.iconStyle]}
-      onPress={props.onPress}
-    />
+    <CustomButton style={buttonStyle} onPress={props.onPress}>
+      <Icon style={iconStyle} name={props.name} />
+    </CustomButton>
   );
 }
